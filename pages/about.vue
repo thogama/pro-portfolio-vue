0647b4b927e8cff0ff3702e7d75bc950ad1a9df7
thogama <dethfiregrasp@gmail.com>
2023-08-12 08:10:55 -0300

    Who section

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,11 @@
 <template>
     <VRow class="h-screen" :class="theme.dark ? 'text-black' : ''">
 
-        <VFadeTransition>
-            <VContainer  v-if="trigger" class="px-xl-10 " style="align-self: center;">
+        <VContainer class="px-xl-10" style="align-self: center;">
+            <VFadeTransition>
 
 
-                <VRow justify="center">
+                <VRow v-if="trigger" justify="center">
                     <VCol cols="6">
 
 
@@ -16,9 +16,8 @@
                         </VCard>
 
                     </VCol>
-                    <VDivider v-if="screen.mdAndUp" vertical/>
-                    <VCol 
-                        class="pt-8 text-h5 special-font text-center d-flex flex-column justify-center align-center">
+                    <VDivider v-if="screen.mdAndUp" vertical />
+                    <VCol class="pt-8 text-h5 special-font text-center d-flex flex-column justify-center align-center">
                         {{ $t("I'm") }}
                         <span class="text-primary text-h1 special-font mx-3">
                             Alan
@@ -28,9 +27,9 @@
 
                             {{ $t("A fullstack developer") }}
                         </span>
-                        <span >
+                        <span>
                             {{ $t("and a") }}
-                            
+
                             <span class="text-primary text-h5">
                                 Vue
                             </span>
@@ -39,19 +38,111 @@
 
                     </VCol>
                 </VRow>
-            </VContainer >
-        </VFadeTransition>
+            </VFadeTransition>
+        </VContainer>
+
+
+    </VRow>
+
+    <VRow class="h-screen">
+        <VContainer class="align-self-center">
+            <div class="px-3 py-5 ">
+                <div :class="theme.dark ? 'text-black' : ''" class="special-font  ps-1 mx-3">
+
+                    {{ $t("Who") }}
+                </div>
+                <div class="text-h2 text-primary py-3 mx-3">
+                    {{ $t("Id") }}
+                </div>
+            </div>
+        </VContainer>
+        <VContainer>
+            <VTable class="border rounded" fixed-header density="comfortable" hover>
+                <thead>
+                    <tr>
+                        <th class="text-left text-h5">
+                            {{ $t("FAQ") }}
+                        </th>
+                        <th class="text-left text-h5">
+                            {{ $t("Answer") }}
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="item in faqs">
+                        <td>{{ $t(item.question) }}</td>
+                        <td class="pa-3" v-if="item.question != 'languages'">{{ item.answer }}</td>
+                        <td v-else>
+                            <VRow align="center">
+
+                                <VCol xs="12" md="10">
+
+                                    <VProgressLinear color="primary" height="5" model-value="100" />
+                                </VCol>
+                                <VCol class="d-flex justify-center align-center">
+
+                                    <Icon size="1.5rem" name="twemoji:flag-brazil" />
+
+                                </VCol>
+
+                            </VRow>
+                            <VRow class="mt-0" align="center">
+
+                                <VCol xs="12" md="10">
+
+                                    <VProgressLinear color="light-blue-darken-1" height="5" model-value="70" />
+                                </VCol>
+                                <VCol class="d-flex justify-center">
+
+                                    <Icon size="1.5rem" name="twemoji:flag-for-flag-united-states" />
+
+                                </VCol>
+
+                            </VRow>
+                        </td>
+                    </tr>
+                </tbody>
+            </VTable>
+        </VContainer>
+
+
     </VRow>
 </template>
-<script setup>
+<script setup lang="ts">
 import { themeStore } from '@/stores/theme';
 import { useDisplay } from 'vuetify/lib/framework.mjs';
 let trigger = ref(false)
 let screen = ref(useDisplay())
+
+function calculateAge(birthDate: string) {
+    const today = new Date();
+    const birthDateObj = new Date(birthDate);
+
+    let age = today.getFullYear() - birthDateObj.getFullYear();
+    const monthDifference = today.getMonth() - birthDateObj.getMonth();
+
+    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDateObj.getDate())) {
+        age--;
+    }
+
+    return age.toLocaleString();
+}
+
 
 onMounted(() => {
     trigger.value = !trigger.value
 })
 
 let theme = themeStore()
+
+let faqs: { question: string, answer: string }[] = [
+    { question: "name", answer: "Alan Jônatas de Oliveira Gama" },
+    { question: "old", answer: calculateAge("2001-11-05") },
+    { question: "where", answer: "Aracaju, Sergipe, Brazil (GMT-3)" },
+    { question: "languages", answer: "" },
+    { question: "exp", answer: calculateAge("2020-01-01") },
+    { question: "time", answer: "40" }
+]
+
+
 </script>
